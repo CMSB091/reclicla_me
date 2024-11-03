e31a5ecb17bd118caf15f757623070e4c59d3b93
Marcelo Saldivar <marcelosaldivar3@gmail.com>
2024-11-03 17:30:12 -0300

    cambios menores

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:camera/camera.dart';
-import 'package:path/path.dart';
 import 'package:recila_me/clases/funciones.dart';
 import 'package:recila_me/widgets/reciclameApp.dart';
 import 'clases/firebase_options.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'dart:io';
 
 List<CameraDescription>? cameras;
 void main() async {
