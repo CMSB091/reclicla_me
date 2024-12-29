70dd924a23305e8be3938219eb6fa5e2eea8a0f7
Marcelo Saldivar <marcelosaldivar3@gmail.com>
2024-12-29 13:31:36 -0300

    Ajustes menores

--- lib/widgets/inicio.dart
+++ lib/widgets/inicio.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
